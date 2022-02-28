6a22eddab3db1308b3bdb56298b3e12d45f986a0
ChoSangHyeon <whtkdgusdldi@naver.com>
2022-02-28 17:16:29 +0900

    [bug fix] unit 테스트 정밀화

--- test/unit/unit.test.js
+++ test/unit/unit.test.js
@@ -10,7 +10,6 @@ const readAuth_middleware = require('../../middlewares/readAuth_middleware');
 User.findOne = jest.fn();
 Post.findAll = jest.fn();
 LikePost.findAll = jest.fn();
-
 let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest();
@@ -110,14 +109,34 @@ describe('getPosts Test', () => {
         req.body = newPost;
     });
     test('모델불러오기', async () => {
+        Post.findAll.mockReturnValue([newPost]);
+        LikePost.findAll.mockReturnValue({});
         await postController.getPosts(req, res);
         expect(res.statusCode).toEqual(200);
         expect(res._isEndCalled).toBeTruthy();
     });
     test('목차불러오기', async () => {
-        Post.findAll.mockReturnValue(newPost);
+        Post.findAll.mockReturnValue([
+            {
+                userId: 'test',
+                postId: 'test',
+                imagePath:
+                    'https://w.namu.la/s/96f05e961ab1169f6f0ef41902388df4832baabd91f44e79ad4b83a2eb53439831f02378ce10fe4073f627f25ac31fcfe3de93161fc5d171902c77b9159aa9178d7e546c91f9dd9c6a9d7e2e70009cac1a7bc74a6c14dda82e9618e3f4fc110cb305848dc8518df8173f38887101fa7d',
+                desc: '사과2',
+            },
+        ]);
+        LikePost.findAll.mockReturnValue([]);
         await postController.getPosts(req, res);
-        console.log('@@@@@@' + res._getData);
-        expect(res._getData()).toStrictEqual(newPost);
+        expect(res._getData()['posts']).toStrictEqual([newPost]);
+    });
+    test('목차불러오기 실패', async () => {
+        Post.findAll.mockReturnValue();
+        LikePost.findAll.mockReturnValue({});
+        await postController.getPosts(req, res);
+        expect(res.statusCode).toEqual(400);
     });
 });
+
+describe('postPosts Test',()=>{
+    
+})
